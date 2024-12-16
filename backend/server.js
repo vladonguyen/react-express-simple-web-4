106363bbcc662fccb98f8fa1e3ed186ef947ca4f
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-12-17 00:54:57 +0200

    Server.js file added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use((req, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type", "Authorization");
+    next();
+});
+
+app.use(bodyParser.json());
+
+const posts = require("./data/posts.js");
+app.use("/posts", posts);
+
 app.listen(3000, ()=> console.log("Server listens on port 3000! Started successfully!"));
